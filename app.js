2d5a09746cb34d893fa24a6ef742e95e4fb1c6a9
Chloe-Woahie <68732833+Chloe-Woahie@users.noreply.github.com>
2024-05-07 11:27:19 -0400

    chore: all finished

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ const port = 8000;
 
 app.use("/", express.static("public"));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
